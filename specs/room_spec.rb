1a631007153b8af037a7a4c5001d22cf43cddb3e
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-05 13:45:17 -0800

    added test for room number but tabling room for now

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -4,18 +4,17 @@ describe "Room class" do
 
   describe "Initializer" do
     it "is an instance of Room" do
-      room = Hotel::Room.new
+      room = Hotel::Room.new(1)
       room.must_be_instance_of Hotel::Room
     end
 
-    # it "creates a list of rooms (numbers)" do
-    #   manager = Hotel::BookingsManager.new
-    #
-    #   manager.must_respond_to :rooms
-    #   manager.rooms.must_be_kind_of Array
-    #   manager.rooms.must_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-    #     14, 15, 16, 17, 18, 19, 20]
-    # end
+    it "has a room number" do
+      room = Hotel::Room.new(1)
+
+      room.must_respond_to :number
+      room.number.must_be_kind_of Integer
+      room.number.must_equal 1
+    end
 
   end
 end
