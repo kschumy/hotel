d7a5c95f1bef06a657fe4e5db4dddca86b6c5a7d
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-05 13:35:39 -0800

    added tests for reader to room numbers list

--- specs/bookings_manager_spec.rb
+++ specs/bookings_manager_spec.rb
@@ -7,7 +7,16 @@ describe "BookingsManager class" do
   describe "Initializer" do
     it "is an instance of BookingsManager" do
       manager = Hotel::BookingsManager.new
-      manager.must_be_kind_of Hotel::BookingsManager
+      manager.must_be_instance_of Hotel::BookingsManager
+    end
+
+    it "creates a list of rooms (numbers)" do
+      manager = Hotel::BookingsManager.new
+
+      manager.must_respond_to :rooms
+      manager.rooms.must_be_kind_of Array
+      manager.rooms.must_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+        14, 15, 16, 17, 18, 19, 20]
     end
 
   end
