637e66ed019ddd974012acf442ad7b52040d068e
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-05 13:38:02 -0800

    added require_relative to room.rb

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -10,3 +10,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
 require_relative '../lib/bookings_manager.rb'
+require_relative '../lib/room.rb'
