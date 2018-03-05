65e92504dd00b35bf1eb175de3fb057139b4a8c8
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-05 14:12:01 -0800

    changed initialize to take a hash

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,10 +1,13 @@
 module Hotel
 
   class Reservation
-      # attr_reader :number
+      attr_reader :id, :room_number, :check_in_date, :check_out_date
 
-    def initialize#(initial_number)
-      # @number = initial_number
+    def initialize(initial_info)
+      @id = initial_info[:id]
+      @room_number = initial_info[:room_number]
+      @check_in_date = initial_info[:check_in_date]
+      @check_out_date = initial_info[:check_out_date]
     end
 
   end
