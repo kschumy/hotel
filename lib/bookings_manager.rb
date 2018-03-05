5a0fdf881f61b86bb7480cd4aaff371c8cfd7cac
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-05 13:35:02 -0800

    has access to room numbers

--- lib/bookings_manager.rb
+++ lib/bookings_manager.rb
@@ -4,9 +4,16 @@
 
 module Hotel
   # ROOM_RATE = 200.0
-  # NUM_OF_ROOMS = 20
+  NUM_OF_ROOMS = 20
+
+
 
   class BookingsManager
+      attr_reader :rooms
+
+    def initialize
+      @rooms = [*1..20]
+    end
 
   end
 end
