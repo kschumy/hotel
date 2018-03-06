74db2ac9c11ed1b30b5c9a83960098031c8d9aea
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-06 13:36:00 -0800

    moded check_if_valid_dates to module method

--- lib/hotel_module.rb
+++ lib/hotel_module.rb
@@ -9,4 +9,21 @@ module Hotel
     end
   end
 
+  # Throw ArgumentError is check_in_date or check_out_date are not Dates or if
+  # check_in_date is not at least one day before check_out_date.
+  def self.check_if_valid_dates(start_date, end_date)
+    has_valid_date_or_error(start_date)
+    has_valid_date_or_error(end_date)
+    if length_of_reservation(start_date, end_date) < 1
+      raise ArgumentError.new("Reservation must be at least one day.")
+    end
+  end
+
+  private
+
+  # Returns the how long the reservation is in days.
+  def self.length_of_reservation(start_date, end_date)
+    return end_date - start_date
+  end
+
 end
