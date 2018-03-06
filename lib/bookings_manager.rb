a4df6ec2b055d514de8d6bf17a0ef58c1a764935
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-05 17:48:07 -0800

    added initial room class

--- lib/bookings_manager.rb
+++ lib/bookings_manager.rb
@@ -9,8 +9,9 @@ module Hotel
       # attr_reader :room, :reservations
 
     def initialize
-      @rooms = [*1..20]
+      @rooms = []
       @reservations = []
+      load_rooms
     end
 
     # Throws ArgumentError if input_check_in or input_check_out are not Dates.
@@ -24,7 +25,13 @@ module Hotel
       return find_reservations_on_date(date)
     end
 
-    # 
+    #
+    # def get_reservations_on_dates(dates)
+    #   Hotel.has_valid_date_or_error(date)
+    #   return find_reservations_on_date(date)
+    # end
+
+    #
     def get_reservation_cost(reservation_id)
       has_valid_reservation_id_or_error(reservation_id)
       return find_reservation_by_id(reservation_id).get_cost
@@ -41,6 +48,12 @@ module Hotel
     end
 
     private
+
+
+    def load_rooms
+      @rooms = []
+      (1..NUM_OF_ROOMS).each { |num| @rooms << Hotel::Room.new(num) }
+    end
 
     def book_a_new_reservation(input_check_in, input_check_out)
       reservation_info = {
@@ -85,22 +98,16 @@ module Hotel
   end
 end
 
-# As an administrator, I can access the list of all of the rooms in the hotel
-# As an administrator, I can reserve a room for a given date range
-# As an administrator, I can access the list of reservations for a specific date
-# As an administrator, I can get the total cost for a given reservation
+# Wave Two: Room Availability
+# User Stories
+# As an administrator, I can view a list of rooms that are not reserved for a
+# given date range
+# As an administrator, I can reserve an available room for a given date range
 
 # Constraints
-# - The hotel has 20 rooms, and they are numbered 1 through 20
-# - Every room is identical, and a room always costs $200/night
-# - The last day of a reservation is the checkout day, so the guest should not be
-#   charged for that night
-# - For this wave, any room can be reserved at any time, and you don't need to
-#   check whether reservations conflict with each other
+# A reservation is allowed start on the same day that another reservation for
+# the same room ends
 
 # Error Handling
-# - Your code should raise an error (StandardError) when an invalid date range
-#   is provided
-
-# Hints
-# You might want to investigate Ruby's Date gem.
+# Your code should raise an exception when asked to reserve a room that is not
+# available
