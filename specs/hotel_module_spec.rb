e3c5f385ea1bba08500aec6993de731d993ab9c3
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-06 13:37:20 -0800

    moved test to check_if_valid_dates to module method

--- specs/hotel_module_spec.rb
+++ specs/hotel_module_spec.rb
@@ -10,7 +10,42 @@ describe "Hotel Module" do
     it "throws an error for an invalid date" do
       proc { Hotel.has_valid_date_or_error("foo") }.must_raise ArgumentError
     end
-
-
   end
+
+  describe "check_if_valid_dates" do
+    it 'returns nil if valid dates' do
+        start_date = Date.new(2018,2,1)
+        end_date = Date.new(2018,2,5)
+        Hotel.check_if_valid_dates(start_date, end_date)
+    end
+
+    it 'throws error if invalid date value' do
+      proc {
+        start_date = Date.new(2018,2,1)
+        end_date = "Look at me. I am the end date now"
+        Hotel.check_if_valid_dates(start_date, end_date)
+      }.must_raise ArgumentError
+
+      proc {
+        start_date = "Look at me. I am the start date now"
+        end_date = Date.new(2018,2,1)
+        Hotel.check_if_valid_dates(start_date, end_date)
+      }.must_raise ArgumentError
+    end
+
+    it 'throws error if date range is not at least one' do
+      proc {
+        start_date = Date.new(2018,2,1)
+        end_date = Date.new(2018,2,1)
+        Hotel.check_if_valid_dates(start_date, end_date)
+      }.must_raise ArgumentError
+
+      proc {
+        start_date = Date.new(2018,2,5)
+        end_date = Date.new(2018,2,4)
+        Hotel.check_if_valid_dates(start_date, end_date)
+      }.must_raise ArgumentError
+    end
+  end
+
 end
