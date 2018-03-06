bd871db91dead52123d6499f0e1da03424f3a9cf
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-05 18:03:19 -0800

    added check for initial room number

--- lib/room.rb
+++ lib/room.rb
@@ -7,6 +7,20 @@ module Hotel
 
     def initialize(initial_number)
       @number = initial_number
+      @reservations = []
+      check_initial_number
+    end
+
+    private
+
+    def check_initial_number
+      if @number.class != Integer || !@number.between?(1, NUM_OF_ROOMS)
+        raise ArgumentError.new("Invalid room number #{@number}")
+      end
+    end
+
+    def add_reservation
+
     end
 
   end
