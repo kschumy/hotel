c512fc6037108eb9f454d7b17bdacd66d46f48c6
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-09 17:06:29 -0800

    created new folder for old files

---
+++ old_version/lib/reservation_old_f_ed_up_verion.rb
@@ -0,0 +1,43 @@
+# # QUESTION: when should I check for valid?
+#
+# module Hotel
+#   @@all_resevations = []
+#
+#   class Reservation
+#       attr_reader :room_number, :check_in, :check_out #, :id
+#
+#     def initialize(initial_info)
+#       # @id = initial_info[:id]
+#       @room_number = initial_info[:room_number]
+#       @check_in = initial_info[:check_in]
+#       @check_out = initial_info[:check_out]
+#       check_if_valid_id
+#       Hotel.check_if_valid_dates(@check_in, @check_out)
+#     end
+#
+#     # Returns the cost, rounded to two decimal places.
+#     def get_cost
+#       return (length_of_reservation(@check_in, @check_out) * ROOM_RATE).round(2)
+#     end
+#
+#     def conflicts_with?(start_date, end_date)
+#       Hotel.check_if_valid_dates(start_date, end_date)
+#       return !(end_date <= @check_in || start_date >= @check_out)
+#     end
+#
+#     private
+#
+#     # Returns the how long the reservation is in days.
+#     def length_of_reservation(start_date = @check_in, end_date = @check_out)
+#       return end_date - start_date
+#     end
+#
+#     # Returns the how long the reservation is in days.
+#     def check_if_valid_id
+#       if @id.class != Integer || @id < 1
+#         raise ArgumentError.new("Invalid Id #{@id}. Must be int > 0")
+#       end
+#     end
+#
+#   end
+# end
---
+++ old_version/specs/reservation_spec_old_f_ed_up_version.rb
@@ -0,0 +1,214 @@
+# require_relative 'spec_helper'
+#
+# describe 'Reservation class' do
+#
+#   describe 'Initializer' do
+#     before do
+#       @initial_id = 42
+#       @initial_room_number = 1
+#       @initial_check_in = Date.new(2018,2,3)
+#       @initial_check_out = Date.new(2018,2,5)
+#
+#       @reservation_info = {
+#         id: @initial_id,
+#         room_number: @initial_room_number,
+#         check_in: @initial_check_in,
+#         check_out: @initial_check_out
+#       }
+#       @reservation = Hotel::Reservation.new(@reservation_info)
+#     end
+#
+#     it 'is an instance of Reservation' do
+#       @reservation.must_be_instance_of Hotel::Reservation
+#     end
+#
+#     it 'has a reservation number' do
+#       @reservation.must_respond_to :id
+#       @reservation.id.must_be_kind_of Integer
+#       @reservation.id.must_equal @initial_id
+#     end
+#
+#     it 'has a room number' do
+#       @reservation.must_respond_to :room_number
+#       @reservation.room_number.must_be_kind_of Integer
+#       @reservation.room_number.must_equal @initial_room_number
+#     end
+#
+#     it 'has a check-in date' do
+#       @reservation.must_respond_to :check_in
+#       @reservation.check_in.must_be_kind_of Date
+#       @reservation.check_in.must_equal @initial_check_in
+#     end
+#
+#     it 'has a check-out date' do
+#       @reservation.must_respond_to :check_out
+#       @reservation.check_out.must_be_kind_of Date
+#       @reservation.check_out.must_equal @initial_check_out
+#     end
+#
+#     it 'throws an error is id is not an Integer' do
+#       proc {
+#         @reservation_info[:id] = 'Look at me. I am the id now.'
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#       proc {
+#         @reservation_info[:id] = nil
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#     end
+#
+#     it 'throws an error is id is less than 1' do
+#       proc {
+#         @reservation_info[:id] = 0
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#     end
+#
+#     it 'throws an error is check-in is not a date' do
+#       proc {
+#         @reservation_info[:check_in] = 'I am not a check-in date.'
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#       proc {
+#         @reservation_info[:check_in] = nil
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#     end
+#
+#     it 'throws an error is check-out is not a date' do
+#       proc {
+#         @reservation_info[:check_out] = 'I am not a check-out date.'
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#       proc {
+#         @reservation_info[:check_out] = nil
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#     end
+#
+#     it 'throws an error if check-in is the same day as check-out' do
+#       proc {
+#         @reservation_info[:check_in] = Date.new(2018,2,5)
+#         @reservation_info[:check_out] = Date.new(2018,2,5)
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#     end
+#
+#     it 'throws an error if check-in is after check-out' do
+#       proc {
+#         @reservation_info[:check_in] = Date.new(2018,2,5)
+#         @reservation_info[:check_out] = Date.new(2018,2,4)
+#         Hotel::Reservation.new(@reservation_info)
+#       }.must_raise ArgumentError
+#     end
+#   end # end of Initializer
+#
+#   describe 'Get Cost' do
+#     before do
+#       @initial_id = 42
+#       @initial_room_number = 1
+#       @initial_check_in = Date.new(2018,2,3)
+#       @initial_check_out = Date.new(2018,2,5)
+#
+#       @reservation_info = {
+#         id: @initial_id,
+#         room_number: @initial_room_number,
+#         check_in: @initial_check_in,
+#         check_out: @initial_check_out
+#       }
+#       @reservation = Hotel::Reservation.new(@reservation_info)
+#     end
+#
+#     it 'returns the cost' do
+#       @reservation.get_cost.must_equal 200.0 * 2
+#     end
+#   end # end of 'Get Cost'
+#
+#
+#   describe 'Conflicts With?' do
+#     before do
+#       @initial_id = 42
+#       @initial_room_number = 1
+#       @initial_check_in = Date.new(2018,2,3)
+#       @initial_check_out = Date.new(2018,2,7)
+#
+#       @reservation_info = {
+#         id: @initial_id,
+#         room_number: @initial_room_number,
+#         check_in: @initial_check_in,
+#         check_out: @initial_check_out
+#       }
+#       @reservation = Hotel::Reservation.new(@reservation_info)
+#     end
+#
+#     it 'returns \'true\' if conflicts with reservation' do
+#       start_date = Date.new(2018,2,2) # ok before but overlaps in mid
+#       end_date = Date.new(2018,2,4)
+#       @reservation.conflicts_with?(start_date, end_date).must_equal true
+#
+#       start_date_2 = Date.new(2018,2,6) # ok after but overlaps in mid
+#       end_date_2 = Date.new(2018,2,9)
+#       @reservation.conflicts_with?(start_date_2, end_date_2).must_equal true
+#
+#       start_date_3 = Date.new(2018,2,5) # fits between start and end dates
+#       end_date_3 = Date.new(2018,2,6)
+#       @reservation.conflicts_with?(start_date_3, end_date_3).must_equal true
+#
+#       start_date_3 = Date.new(2018,2,1) # completely overlaps other reservation
+#       end_date_3 = Date.new(2018,2,9)
+#       @reservation.conflicts_with?(start_date_3, end_date_3).must_equal true
+#     end
+#
+#     it 'returns \'false\' if does not conflict with reservation' do
+#       start_date = Date.new(2018,2,1)
+#       end_date = Date.new(2018,2,2) # ends day before
+#       @reservation.conflicts_with?(start_date, end_date).must_equal false
+#
+#       start_date_2 = Date.new(2018,2,8) # starts day after
+#       end_date_2 = Date.new(2018,2,9)
+#       @reservation.conflicts_with?(start_date_2, end_date_2).must_equal false
+#     end
+#
+#     it 'returns \'false\' with overlap on start and end dates' do
+#       start_date = Date.new(2018,2,1)
+#       end_date = Date.new(2018,2,3) # ends on day of check-in
+#       @reservation.conflicts_with?(start_date, end_date).must_equal false
+#
+#       start_date_2 = Date.new(2018,2,7) # starts on day of check-out
+#       end_date_2 = Date.new(2018,2,8)
+#       @reservation.conflicts_with?(start_date_2, end_date_2).must_equal false
+#     end
+#
+#     it 'throws error if invalid date value' do
+#       proc {
+#         start_date = Date.new(2018,2,1)
+#         end_date = "Look at me. I am the end date now"
+#         @reservation.conflicts_with?(start_date, end_date)
+#       }.must_raise ArgumentError
+#
+#       proc {
+#         start_date = "Look at me. I am the start date now"
+#         end_date = Date.new(2018,2,1)
+#         @reservation.conflicts_with?(start_date, end_date)
+#       }.must_raise ArgumentError
+#     end
+#
+#     it 'throws error if date range is not at least one' do
+#       proc {
+#         start_date = Date.new(2018,2,1)
+#         end_date = Date.new(2018,2,1)
+#         @reservation.conflicts_with?(start_date, end_date)
+#       }.must_raise ArgumentError
+#
+#       proc {
+#         start_date = Date.new(2018,2,5)
+#         end_date = Date.new(2018,2,4)
+#         @reservation.conflicts_with?(start_date, end_date)
+#       }.must_raise ArgumentError
+#     end
+#   end # end of 'Available During Range?'
+#
+#
+#
+#
+# end # end of Reservation
