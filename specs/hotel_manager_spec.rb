6213861f2f679aed2e490f60a83e52c0cbe720ad
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-11 00:50:50 -0800

    moved over old reservation tests and modified to meet current program

--- specs/hotel_manager_spec.rb
+++ specs/hotel_manager_spec.rb
@@ -13,7 +13,6 @@ describe 'HotelManager class' do
     end
   end # end of 'Initializer'
 
-
   describe 'Get Rooms' do
     before do
       @manager = Hotel::HotelManager.new
@@ -21,17 +20,25 @@ describe 'HotelManager class' do
 
     it 'returns a list of rooms' do
       @manager.must_respond_to :rooms
-      @manager.rooms.must_be_kind_of Array
-      @manager.rooms.must_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-        14, 15, 16, 17, 18, 19, 20]
+      @manager.rooms.must_be_kind_of Hash
+      @manager.rooms.keys.must_equal [:'1', :'2', :'3', :'4', :'5', :'6', :'7',
+        :'8', :'9', :'10', :'11', :'12', :'13', :'14', :'15', :'16', :'17',
+        :'18', :'19', :'20']
       @manager.rooms.size.must_equal 20
     end
 
     it 'preserves the original list of rooms' do
-      puts Hotel.all_resevations.inspect
-      @manager.rooms.pop
-      @manager.rooms.must_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-        14, 15, 16, 17, 18, 19, 20]
+      @manager.rooms.delete(:'1')
+      @manager.rooms.keys.must_equal [:'1', :'2', :'3', :'4', :'5', :'6', :'7',
+        :'8', :'9', :'10', :'11', :'12', :'13', :'14', :'15', :'16', :'17',
+        :'18', :'19', :'20']
+    end
+
+    it 'preserves the original list of rooms' do
+      @manager.rooms.delete(:'1')
+      @manager.rooms.keys.must_equal [:'1', :'2', :'3', :'4', :'5', :'6', :'7',
+        :'8', :'9', :'10', :'11', :'12', :'13', :'14', :'15', :'16', :'17',
+        :'18', :'19', :'20']
     end
   end
 
@@ -41,17 +48,13 @@ describe 'HotelManager class' do
     end
 
     it 'reserves a room' do
-      @manager.must_respond_to :rooms
-      @manager.rooms.must_be_kind_of Array
-      @manager.rooms.must_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-        14, 15, 16, 17, 18, 19, 20]
-      @manager.rooms.size.must_equal 20
+      @manager.must_respond_to :reserve_room
     end
 
     # it 'preserves the original list of rooms' do
     #   @manager.rooms.pop
-    #   @manager.rooms.must_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-    #     14, 15, 16, 17, 18, 19, 20]
+    #   @manager.rooms.must_equal [1 2 3 4 5 6 7 8 9 10 11 12 13
+    #     14 15 16 17 18 19 20]
     # end
   end
 
@@ -70,10 +73,10 @@ describe 'HotelManager class' do
   #
   #   it 'returns a copy of reservations' do
   #     reservation_info = {
-  #       id: 1,
-  #       room_number: 3,
-  #       check_in: Date.new(2018,2,3),
-  #       check_out: Date.new(2018,2,5)
+  #       id: 1
+  #       room_number: 3
+  #       check_in: Date.new(201823)
+  #       check_out: Date.new(201825)
   #     }
   #     reservation = Hotel::Reservation.new(reservation_info)
   #
@@ -88,10 +91,10 @@ describe 'HotelManager class' do
   #   before do
   #     @manager = Hotel::Room.new(3)
   #     reservation_info = {
-  #       id: 1,
-  #       room_number: 3,
-  #       check_in: Date.new(2018,2,3),
-  #       check_out: Date.new(2018,2,5)
+  #       id: 1
+  #       room_number: 3
+  #       check_in: Date.new(201823)
+  #       check_out: Date.new(201825)
   #     }
   #     @reservation = Hotel::Reservation.new(reservation_info)
   #
@@ -108,8 +111,8 @@ describe 'HotelManager class' do
   #
   #   it 'throws error if not available for reservation' do
   #     proc {
-  #       new_reservation = Hotel::Reservation.new(id: 14, room_number: 3,
-  #       check_in: Date.new(2018,2,1), check_out: Date.new(2018,2,5))
+  #       new_reservation = Hotel::Reservation.new(id: 14 room_number: 3
+  #       check_in: Date.new(201821) check_out: Date.new(201825))
   #       @manager.add_reservation(new_reservation)
   #     }.must_raise ArgumentError
   #   end
@@ -120,39 +123,39 @@ describe 'HotelManager class' do
   # describe 'Is Available?' do
   #   before do
   #     @manager = Hotel::Room.new(3)
-  #     @manager.add_reservation(Hotel::Reservation.new({id: 21, room_number: 12,
-  #       check_in: Date.new(2018,2,3), check_out: Date.new(2018,2,8)}))
-  #     @manager.add_reservation(Hotel::Reservation.new({id: 83, room_number: 12,
-  #       check_in: Date.new(2018,2,12), check_out: Date.new(2018,2,17)}))
+  #     @manager.add_reservation(Hotel::Reservation.new({id: 21 room_number: 12
+  #       check_in: Date.new(201823) check_out: Date.new(201828)}))
+  #     @manager.add_reservation(Hotel::Reservation.new({id: 83 room_number: 12
+  #       check_in: Date.new(2018212) check_out: Date.new(2018217)}))
   #   end
   #
   #   it 'returns true if is available in range' do
-  #     @manager.is_available?(Date.new(2018,2,9), Date.new(2018,2,11)).must_equal true
+  #     @manager.is_available?(Date.new(201829) Date.new(2018211)).must_equal true
   #   end
   #
   #   it 'returns true if available on another reservation end date' do
-  #     @manager.is_available?(Date.new(2018,2,8), Date.new(2018,2,11)).must_equal true
+  #     @manager.is_available?(Date.new(201828) Date.new(2018211)).must_equal true
   #   end
   #
   #   it 'returns true if end date is on another reservation start date' do
-  #     @manager.is_available?(Date.new(2018,2,9), Date.new(2018,2,12)).must_equal true
+  #     @manager.is_available?(Date.new(201829) Date.new(2018212)).must_equal true
   #   end
   #
   #   it 'returns true of starts/ends overlap with two reservations' do
-  #     @manager.is_available?(Date.new(2018,2,8), Date.new(2018,2,12)).must_equal true
+  #     @manager.is_available?(Date.new(201828) Date.new(2018212)).must_equal true
   #   end
   #
   #   it 'returns false if overlaps on non-start/end dates' do
-  #     @manager.is_available?(Date.new(2018,2,9), Date.new(2018,2,13)).must_equal false
+  #     @manager.is_available?(Date.new(201829) Date.new(2018213)).must_equal false
   #   end
   #
   #   it 'returns false if overlaps on non-start/end dates' do
-  #     @manager.is_available?(Date.new(2018,2,7), Date.new(2018,2,11)).must_equal false
+  #     @manager.is_available?(Date.new(201827) Date.new(2018211)).must_equal false
   #   end
   #
   #   # it 'throws error if the reservation is not a Reservation' do
   #   #   proc {
-  #   #     @manager.is_available?("foo", Date.new(2018,2,11))
+  #   #     @manager.is_available?("foo" Date.new(2018211))
   #   #   }.must_raise ArgumentError
   #   # end
   # end # end of 'Is Available?'
