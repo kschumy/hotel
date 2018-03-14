cdabbcb51e677891843fda074307863f6b6ad469
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-13 18:10:43 -0700

    moved methods dealing with reservation logic around

--- lib/hotel_manager.rb
+++ lib/hotel_manager.rb
@@ -3,61 +3,64 @@
 module Hotel
 
   NUM_OF_ROOMS = 20
-  STANDARD_RATE = 200.0
+  ROOMS = (1..NUM_OF_ROOMS).map { |num| num }
+  RATE = 200.0
 
   class HotelManager
     include ReservationDates
+    include Reservable
 
     def initialize
-      @rooms = build_rooms
-      # @reservations = Hotel::Reservation.get_all_reservations
+      @reservations = []
+      @blocks = []
     end
 
-    def get_available_rooms(start_date, end_date)
-      date_range = get_date_range(start_date, end_date)
-      return @rooms.select { |room| room if room.is_available?(date_range) }
+    def get_available_rooms(check_in, check_out)
+      res_in_range = get_reservations_in_range(check_in: check_in,
+        check_out: check_out)
+      return get_rooms - res_in_range.collect { |res| res.room }
     end
 
     def get_reservations
-      return Hotel::Reservation.get_all_reservations
+      return @reservations.dup
     end
 
     def get_reservations_on_date(date)
-      one_day_range = get_date_range(date)
-      return reservations.select { |res| res if conflicts_with_dates?(res, one_day_range) }
+      return get_reservations_in_range(check_in: date)
     end
 
-    def reserve_room(start_date, end_date)
-      date_range = get_date_range(start_date, end_date)
-      room = get_available_room(date_range)
-      room.add_reservation(Reservation.new({room: room.number,
-        check_in: date_range.check_in, check_out: date_range.check_out}))
+    def reserve_room(check_in, check_out)
+      room_num = get_available_rooms(check_in, check_out).first
+      @reservations << Reservation.new({room: room_num,
+        check_in: check_in, check_out: check_out})
     end
 
-    # Returns a list of room numbers.
-    def rooms
-      return @rooms.dup
+    def reserve_room(check_in, check_out)
+      @reservations << reserve_a_room(check_in, check_out)
+    end
+
+    def get_rooms
+      return ROOMS.dup
     end
 
     private
 
-    def get_date_range(start_date, end_date = nil)
-      has_date_or_error(start_date)
-      end_date.nil? ? end_date = start_date.next_day : has_date_or_error(end_date)
-      return DateRange.new(start_date, end_date)
+    def get_reservations_in_range(check_in:, check_out:)
+      range = get_date_range(check_in: check_in, check_out: check_out)
+      puts range.inspect
+      return get_reservations.select { |res| res if conflicts_with_dates?(res, range) }
     end
 
-    def get_available_room(date_range)
-      return @rooms.find { |room| room if room.is_available?(date_range) }
+    def get_date_range(check_in:, check_out:)
+      has_date_or_error(check_in)
+      check_out.nil? ? check_out = check_in.next_day : has_date_or_error(check_out)
+      return DateRange.new(check_in, check_out)
     end
 
-    def build_rooms
-      return Array.new(NUM_OF_ROOMS) { |num| Room.new(num + 1) }
-    end
+    # def burn_it_to_the_ground
+    #   exit
+    # end
 
-    def burn_it_to_the_ground
-      exit
-    end
 
   end
 end
--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,27 +1,34 @@
 require_relative 'reservation_dates'
+require_relative 'reservable'
+
 
 module Hotel
   class Reservation
 
     include ReservationDates
+    include Reservable
 
-    @@all_reservations = []
+    # @@all_reservations = []
 
-    attr_reader :id, :room, :check_in, :check_out, :cost
+    attr_reader :id, :room, :check_in, :check_out, :cost #:id,
 
     def initialize(initial_info)
-      @id = @@all_reservations.size + 1
+      # @id = @@all_reservations.size + 1
       @room = initial_info[:room]
       @check_in = initial_info[:check_in]
       @check_out = initial_info[:check_out]
       @cost = get_cost(initial_info.fetch(:rate, 200.0))
-      @@all_reservations << self
+      # @@all_reservations << self
     end
 
 
-    def self.get_all_reservations
-      return @@all_reservations.dup
-    end
+    # def self.get_all_reservations
+    #   return @@all_reservations.dup
+    # end
+
+    # def self.get_reservations_on_range
+    #   return get_all_reservations.select { |res| res if conflicts_with_dates?(res, range) }
+    # end
 
     private
 
