5b7e6812c7e6b055eca7e2bbaa35db3ca0a7d8dd
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-13 18:09:46 -0700

    made reservable module which may not be used

---
+++ lib/reservable.rb
@@ -0,0 +1,24 @@
+module Reservable
+
+  def get_available_rooms(check_in, check_out)
+    res_in_range = get_reservations_in_range(check_in: check_in,
+      check_out: check_out)
+    return get_rooms - res_in_range.collect { |res| res.room }
+  end
+
+  def get_reservations
+    return @reservations.dup
+  end
+
+  def get_reservations_on_date(date)
+    return get_reservations_in_range(check_in: date)
+  end
+
+  def reserve_a_room(check_in, check_out, res_id)
+    room_num = get_available_rooms(check_in, check_out).first
+    return {room: room_num, check_in: check_in, check_out: check_out}
+  end
+
+
+
+end
