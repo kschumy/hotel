badfbcfd7c877f669faeb2303769c48678e59eb8
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-15 00:18:55 -0700

    added logic for reservable module

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -10,6 +10,6 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative '../lib/reservation.rb'
 require_relative '../lib/hotel_manager.rb'
 require_relative '../lib/reservation_dates.rb'
-# require_relative '../lib/reservable.rb'
+require_relative '../lib/reservable.rb'
 require_relative '../lib/block.rb'
 require_relative '../lib/dates_range.rb'
