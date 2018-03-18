65fd9f85d82afec3baf66e9eedc6a88e1ad0b70b
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-18 16:50:34 -0700

    moved files around

---
+++ old_version/lib/reservable.rb
@@ -0,0 +1,29 @@
+# # Class that uses this module must
+# module Reservable
+#
+#   def get_reservations
+#     return @reservations.dup
+#   end
+#
+#   def get_available_rooms(check_in, check_out)
+#     reservations_in_range = find_reservations_in_range(check_in: check_in,
+#       check_out: check_out)
+#     return get_rooms - reservations_in_range.collect { |res| res.room }
+#   end
+#
+#   private
+#
+#   def get_new_reservation_info(check_in, check_out)
+#     room_num = get_available_rooms(check_in, check_out).sample
+#     raise ArgumentError.new("No room available") if room_num.nil?
+#     return {room: room_num, check_in: check_in, check_out: check_out}
+#   end
+#
+#   def find_reservations_in_range(check_in:, check_out: nil, res_list: get_reservations)
+#     return res_list.select { |res|
+#       res if !res.is_held? && res.conflicts_with?(check_in: check_in,
+#         check_out: check_out) }
+#   end
+#
+#
+# end
